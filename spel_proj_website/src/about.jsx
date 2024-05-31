773efef351e7fa5cc00a6ab019f8dc5913365c7c
silverknet <70332569+silverknet@users.noreply.github.com>
2024-05-31 18:17:21 +0200

    added images

--- spel_proj_website/src/about.jsx
+++ spel_proj_website/src/about.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import React from 'react';
 import bad_ground from './assets/bad_ground.png'
+import pic_palak from './assets/pic_palak.jpg';
+import martin from './assets/martin.jpeg';
+import m from './assets/m.jpeg';
+import n from './assets/n.jpg';
 
 const About = React.forwardRef((props, ref) => {
   return (
@@ -19,8 +23,24 @@ const About = React.forwardRef((props, ref) => {
             <div className='TextSection'>
                 <h2>About the Team</h2>
                 <p> The team is an elite group of 4 individuals studying at KTH Royal Institute of Technoloy in Sweden. This group combined their skills and strenghts to create a masterpiece you must play.  </p>
-                <img className='ImageSection' src="pic_palak.jpg" alt=""></img>
-                <img className='ImageSection' src="martin.jpg" alt=""></img>
+                <div className='ImageSection'>
+                  <div className='ImageContainer2'>
+                  <span className='ImageLabel'>Palakk</span>
+                    <img className='Image' src={pic_palak} alt="Palak" />
+                  </div>
+                  <div className='ImageContainer2'>
+                    <img className='Image' src={martin} alt="Martin" />
+                    <span className='ImageLabel'>Martin</span>
+                  </div>
+                  <div className='ImageContainer2'>
+                    <span className='ImageLabel'>Malki</span>
+                    <img className='Image' src={m} alt="M" />
+                  </div>
+                  <div className='ImageContainer2'>
+                    <img className='Image' src={n} alt="N" />
+                    <span className='ImageLabel'>Natalia</span>
+                  </div>
+                </div>
             </div>
         </div>
         <div className='ImageContainer'>
