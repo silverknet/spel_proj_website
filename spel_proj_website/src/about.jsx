7622d287734ec0bc8db75a73c2820b7d127b9b32
Palak Khera Khera <khera@kth.se>
2024-05-31 07:27:54 -0500

    website content and a couple pictures

--- spel_proj_website/src/about.jsx
+++ spel_proj_website/src/about.jsx
@@ -7,12 +7,20 @@ const About = React.forwardRef((props, ref) => {
     <div className="About" ref={ref}>
       <div className='AboutPageContent'>
             <div className='TextSection'>
-                <h2>Section 1</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus varius nisl vel est finibus, placerat bibendum neque viverra. Praesent facilisis ut lectus a feugiat. Aliquam quis tortor suscipit, porttitor nunc sit amet, rutrum est. Aenean nec efficitur tortor, vitae ultricies nibh. Aliquam augue erat, vulputate at eros a, lobortis venenatis augue. Pellentesque cursus mattis neque, finibus luctus neque. Pellentesque faucibus nec dolor eget congue. <br/> <br/>Ut laoreet odio eget ipsum hendrerit, sit amet consectetur massa bibendum. Aliquam interdum nisl quis nulla hendrerit varius. Curabitur pharetra arcu et nisi ultricies commodo. Mauris et mi ut quam tincidunt iaculis vitae at dui. Aenean elementum volutpat varius. Ut ut maximus diam. Morbi ut interdum arcu. Donec sapien enim, sodales ut orci id, convallis accumsan neque. Fusce faucibus elit vitae auctor efficitur.</p>
+                <h2>Features</h2>
+                <p> - Dual-World Exploration: Seamlessly transitions between the good and the bad dimensions.
+                <p> - Resource Management: The player has to collect bullets and other resources in the good world to arm itself against the dangers of the upside down world.</p>
+                <p> - Shooting Mechanics: As the player progresses through the upside down world, they engage in encounters with enemies and need to use the collected bullets to fight them off.</p>
+                <p> - Environmental Puzzles: The player needs to solve environmental puzzles and overcome obstacles to progress through the game.</p>
+                <p> - Evolving Narrative: Uncover the mysteries of the parallel worlds through a storyline filled with twists, turns, and unexpected revelations.</p>
+                </p>
+                 
             </div>
             <div className='TextSection'>
-                <h2>Section 1</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus varius nisl vel est finibus, placerat bibendum neque viverra. Praesent facilisis ut lectus a feugiat. Aliquam quis tortor suscipit,<br/> <br/>porttitor nunc sit amet, rutrum est. Aenean nec efficitur tortor, vitae ultricies nibh. Aliquam augue erat, vulputate at eros a, lobortis venenatis augue. Pellentesque cursus mattis neque, finibus luctus neque. Pellentesque faucibus nec dolor eget congue. Ut laoreet odio eget ipsum hendrerit, sit amet consectetur massa bibendum. Aliquam interdum nisl quis nulla hendrerit varius. Curabitur pharetra arcu et nisi ultricies commodo. Mauris et mi ut quam tincidunt iaculis vitae at dui. Aenean elementum volutpat varius. Ut ut maximus diam. Morbi ut interdum arcu. Donec sapien enim, sodales ut orci id, convallis accumsan neque. Fusce faucibus elit vitae auctor efficitur.</p>
+                <h2>About the Team</h2>
+                <p> The team is an elite group of 4 individuals studying at KTH Royal Institute of Technoloy in Sweden. This group combined their skills and strenghts to create a masterpiece you must play.  </p>
+                <img className='ImageSection' src="pic_palak.jpg" alt=""></img>
+                <img className='ImageSection' src="martin.jpg" alt=""></img>
             </div>
         </div>
         <div className='ImageContainer'>
