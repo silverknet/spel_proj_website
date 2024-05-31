ed6c51d3dcc21636279c482ea29ab92b454fea27
silverknet <70332569+silverknet@users.noreply.github.com>
2024-05-31 18:24:44 +0200

    replaced game with trailer for deploy

--- spel_proj_website/src/game.jsx
+++ spel_proj_website/src/game.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import React from 'react';
 
-
+/*        <iframe src="./putgamehere/index.html" style={{ width: '956px', height: '557px' }}></iframe>*/
 
 const Game = React.forwardRef((props, ref) => {
   return (
     <div className="Game" ref={ref}>
-        <iframe src="./putgamehere/index.html" style={{ width: '956px', height: '557px' }}></iframe>
+        <iframe 
+            src="https://www.youtube.com/embed/GeDwJKJAz70" 
+            style={{ width: '956px', height: '557px' }} 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen
+        ></iframe>
     </div>
+    
   )
 });
 
